77958ae9cba28ab2ced7cf3178cf3936dc6e45da
Allison Payne <allisonpayne01@gmail.com>
2026-02-15 19:02:12 -0800

    distance matrix for dive shape

--- scripts/3_dtw.R
+++ scripts/3_dtw.R
@@ -4,16 +4,16 @@ library(dtw)
 db_dives <- read_rds(here::here("output/all_db_dives_processed.rds"))
 
 #re-calculating the dive id now that they are combined
-db_dives <- db_dives %>% 
+db_dives_75 <- db_dives %>% 
   mutate(dive_id = cumsum(Time - lag(Time, default = Time[1]) > 1.01)) %>% 
   group_by(dive_id) %>% 
-  #only take the middle 50% of the dive
-  slice(floor(n() / 4):floor(n() * 3 / 4)) %>% 
+  #only take the middle 80% of the dive
+  slice(floor(n() * 0.125):floor(n() * 0.875)) %>% 
   ungroup()
 
-dive_ids <- unique(db_dives$dive_id)
+dive_ids <- unique(db_dives_75$dive_id)
 n_dives <- length(dive_ids)
-dive_list <- db_dives %>% 
+dive_list <- db_dives_75 %>% 
   select(dive_id, db_1000_3500) %>% 
   group_split(dive_id)
 
@@ -27,29 +27,30 @@ for (i in 1:(n_dives - 1)) {
   }
 }
 
-write_rds(x = dtw_dist_mtx, 
-          file = "output/dtw_dist_mtx.rds")
-
 rownames(dtw_dist_mtx) <- dive_ids
 dtw_dist <- as.dist(dtw_dist_mtx)
 
-#plotting, seeing if this worked
-plot(hclust(dtw_dist), )
-clust_tree <- hclust(dtw_dist)
-clusts <- cutree(clust_tree, h = 6000)
+write_rds(x = dtw_dist, 
+          file = "output/dtw_dist_mtx_75.rds")
 
 #how does it look with the dives?
 
 dives <- read_rds(here::here("output/processed_dives.rds"))
 dive_summ <- read_rds(here::here("output/processed_dive_summaries.rds"))
 recording <- read_rds(here::here("output/recording_calib.rds"))
+dtw_dist_75 <- read_rds(here::here("output/dtw_dist_mtx_75.rds"))
+# REMOVE ME after rerunning the distance matrix code above
+rownames(dtw_dist_75) <- dive_ids
+dtw_dist_75 <- as.dist(dtw_dist_75)
 
+rownames(dtw_dist_mtx) <- dive_ids
+dtw_dist <- as.dist(dtw_dist_mtx)
 
-dive_summ <- dive_summ %>%    
-  group_by(dive_id) 
+clust_tree <- hclust(dtw_dist_75)
+clusts <- cutree(clust_tree, k = 8)
 
 # Associate dives between acoustics and depth profiles
-dive_clusts <- db_dives %>% 
+dive_clusts <- db_dives_75 %>% 
   group_by(dive_id) %>% 
   summarize(start = first(Time),
             end = last(Time)) %>% 
@@ -60,16 +61,58 @@ dive_clusts <- db_dives %>%
              seq_along(recording$end),
              xout = end,
              method = "constant",
-             f = 1)$y
+             f = 1,
+             rule = 2)$y
     ],
     dive_id = approx(dive_summ$end,
                      dive_summ$dive_id,
                      xout = recording_end,
                      method = "constant",
-                     f = 1)$y)
+                     f = 1,
+                     rule = 2)$y)
+
+# drop second recording from within a dive (e.g. due to inversion)
+dive_clusts <- dive_clusts %>% 
+  group_by(dive_id) %>% 
+  slice(1) %>% 
+  ungroup()
+
+# Run depth profile DTW
+dive_profile_list <- dives %>% 
+  semi_join(dive_clusts, by = "dive_id") %>% 
+  group_by(dive_id) %>% 
+  mutate(depth_std = Depth / max(Depth)) %>% 
+  ungroup() %>% 
+  select(dive_id, depth_std) %>% 
+  group_split(dive_id)
+n_profiles <- length(dive_profile_list)
+dtw_depth_mtx <- matrix(0, nrow = n_profiles, ncol = n_profiles)
+for (i in 1:(n_profiles - 1)) {
+  print(str_glue("{i} / {(n_profiles - 1)}"))
+  for (j in (i + 1):n_profiles) {
+    dtw_depth_mtx[i, j] <- dtw(dive_profile_list[[i]]$depth_std, 
+                               dive_profile_list[[j]]$depth_std)$distance
+    dtw_depth_mtx[j, i] <- dtw_depth_mtx[i, j]
+  }
+}
+rownames(dtw_depth_mtx) <- unique(dive_clusts$dive_id)
+dtw_depth <- as.dist(dtw_depth_mtx)
+
+# Standardize distance matrices, combine, and apply clustering
+w <- c(0.5, 0.5) # equally weighted
+dtw_depth_std <- dtw_depth / max(dtw_depth)
+dtw_dist_75_std <- dtw_dist_75 / max(dtw_dist_75)
+combined_dtw <- w[1] * dtw_depth_std + w[2] * dtw_dist_75_std
+combined_tree <- hclust(combined_dtw)
+clusts <- cutree(combined_tree, k = 12)
+
+# Max wants to check this when DTW on depth profiles finish
+dive_clusts$clust <- clusts
 
 dives %>% 
-  right_join(select(dive_clusts, dive_id, clust), by = "dive_id") %>% 
+  right_join(select(dive_clusts, dive_id, clust), 
+             by = "dive_id",
+             relationship = "many-to-one") %>% 
   group_by(dive_id) %>% 
   mutate(elapsed = as.numeric(Date - first(Date), unit = "secs")) %>% 
   ungroup() %>% 
@@ -99,6 +142,13 @@ dives %>%
   filter(dive_id == 710:713) %>% 
   ggplot(aes(Date, Depth)) +
   geom_line(linewidth = 1) +
+  scale_y_reverse() +
+  theme_bw()
+
+db_dives %>% 
+  filter(dive_id == 710:713) %>% 
+  ggplot(aes(Time, db_1000_3500)) +
+  geom_line(linewidth = .5) +
   scale_y_reverse() +
   theme_bw()
 
