ddc1bfcaa71e6a3b59fc679ffff6896c46c3a54d
Allison Payne <allisonpayne01@gmail.com>
2026-03-04 10:12:17 -0800

    Adding the 3 state HMM to the main medium-sw report.

--- scripts/generate_dive_plots.R
+++ scripts/generate_dive_plots.R
@@ -1,4 +1,20 @@
 library(gridExtra)
 library(tidyverse)
+source(here::here("R/dive_type_plots.R"))
 
 dives <- read_rds(here::here("output/processed_dives.rds"))
+recording <- readRDS(here::here("output/recording_calib.rds"))
+
+recording_start <- recording$start_calib[1]
+recording_end <- last(recording$end_calib)
+
+#filter dives to only when we had recordings
+dives <- dives %>% 
+  filter(between(Date, recording_start, recording_end))
+
+#Generate plots and save to outputs folder
+walk(unique(last_dives$dive_id),
+     \(fd) {
+       dive_type_plot(dives, fd, -2:2)
+       Sys.sleep(0.01)  # Small delay between plots to reduce likelihood of crashing partway through (due to memory issue)
+     })
